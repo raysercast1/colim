97e949f4517b0805dc8bf48c94aa80b4f2e24923
Rayser <rayser.alejandro@gmail.com>
2020-06-06 15:35:08 -0500

    feat(api): add config file to config database

--- dist/config/typeorm.config.js
+++ dist/config/typeorm.config.js
@@ -1,14 +1,28 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.typeOrmConfig = void 0;
+const config_1 = __importDefault(require("config"));
+const type = config_1.default.get('db.type');
+const host = config_1.default.get('db.host');
+const port = config_1.default.get('db.port');
+const username = config_1.default.get('db.username');
+const password = config_1.default.get('db.password');
+const database = config_1.default.get('db.database');
+const synchronize = config_1.default.get('db.synchronize');
 exports.typeOrmConfig = {
-    type: 'postgres',
-    host: '172.17.0.2',
-    port: 5432,
-    username: 'colimptexdb',
-    password: 'postgres',
-    database: 'colimptex',
+    type: type,
+    host: host,
+    port: port,
+    username: username,
+    password: password,
+    database: database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    migrationsTableName: 'migrations_typeorm',
+    migrations: ['dist/migrations/*{.ts,.js}'],
+    migrationsRun: true,
+    synchronize: synchronize,
 };
 //# sourceMappingURL=typeorm.config.js.map
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,29 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
+import config from 'config';
+
+const type: any = config.get('db.type');
+const host: string = config.get('db.host');
+const port: number = config.get('db.port');
+const username: any = config.get('db.username');
+const password: any = config.get('db.password');
+const database: string = config.get('db.database');
+const synchronize: boolean = config.get('db.synchronize');
+
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
 
-    type: 'postgres',
-    host: '172.17.0.2',
-    port: 5432,
-    username: 'colimptexdb',
-    password: 'postgres',
-    database: 'colimptex',
+    type: type,
+    host: host,
+    port: port,
+    username: username,
+    password: password,
+    database: database,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
+    migrationsTableName: 'migrations_typeorm',
+    migrations: ['dist/migrations/*{.ts,.js}'],
+    migrationsRun: true,
+    // // cli: {
+    // //     migrationsDir: 'migration'
+    // // },
+    synchronize: synchronize,
 }
